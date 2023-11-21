c7d6f5e2c9a28e556c39deb2797425f369c80b17
Chakir Manyao Jija <84908567+Student5427@users.noreply.github.com>
2023-11-21 10:59:17 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -4,7 +4,8 @@
 
 int main()
 {
-    double *arr = find_roots(3,1,2);
+    double* roots = (double*)malloc(2 * sizeof(double)); 
+    find_roots(roots, 3, 1, 2);
     printf("Hello World!\n");
     printf("x1 = %lf, x2 = %lf, number = %lf", arr[0], arr[2], arr[3]);
     return myfunc(2);
