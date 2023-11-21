7221908e9bee4e49601146f01ca4c83276cd2135
Chakir Manyao Jija <84908567+Student5427@users.noreply.github.com>
2023-11-21 11:19:08 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -6,7 +6,6 @@ int main()
 {
     double* roots = (double*)malloc(2 * sizeof(double)); 
     find_roots(roots, 3, 1, 2);
-    printf("Hello World!\n");
     printf("x1 = %lf, x2 = %lf",  roots[0], roots[1]);
     free(roots);
     return 0;
