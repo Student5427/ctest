67ece129b39a53fd97fe55937f1b28ca22c6a08c
Chakir Manyao Jija <84908567+Student5427@users.noreply.github.com>
2023-11-28 08:41:34 +0300

    Update myfunc.h

--- app/myfunc.h
+++ app/myfunc.h
@@ -1,11 +1,13 @@
 #ifndef MYFUNC_H
 #define MYFUNC_H
 
+void myfunc(double* roots, double a, double b, double c);
 
 int fibonachi(int num);
 
-void find_roots(double* roots,double a,double b,double c);
+void printStdoutMessages();
 
 extern int val;
+
 
 #endif // MYFUNC_H
