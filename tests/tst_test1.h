f00ad75338044d073b034a4f0261c44c46c77a26
Chakir Manyao Jija <84908567+Student5427@users.noreply.github.com>
2023-11-28 08:49:16 +0300

    Update tst_test1.h

--- tests/tst_test1.h
+++ tests/tst_test1.h
@@ -1,13 +1,51 @@
+#ifndef MYTEST_H
+#define MYTEST_H
+
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
+
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <math.h>
 
 /* using namespace testing; */
 extern "C" {
 #include "myfunc.h"
 }
-
-TEST(test1, suite1)
+TEST(mytestB, root2)
 {
-    EXPECT_EQ(1, 1);
-    /*ASSERT_THAT(0, Eq(0)); */
+    double* roots = (double*)malloc(2 * sizeof(double));
+    myfunc(roots, 4, -1, -5);
+    ASSERT_EQ(roots[0], 1.25);
+    ASSERT_EQ(roots[1], -1.00);
+    free(roots);
 }
+
+TEST(mytestB, root1)
+{
+    double* roots = (double*)malloc(2 * sizeof(double));
+    myfunc(roots, 1, 12, 36);
+    ASSERT_EQ(roots[0], -6.00);
+    ASSERT_TRUE(isnan(roots[1]));
+    free(roots);
+}
+
+TEST(mytestB, root0)
+{
+    double* roots = (double*)malloc(2 * sizeof(double));
+    myfunc(roots, 2, 1, 67);
+    ASSERT_TRUE(isnan(roots[0]));
+    ASSERT_TRUE(isnan(roots[1]));
+    free(roots);
+}
+
+TEST(mytestI, integrated)
+{
+    double* roots = (double*)malloc(2 * sizeof(double));
+    myfunc(roots, 1, -6, 9);
+    ASSERT_EQ(fibonachi((int)roots[0]), 2);
+    free(roots);
+}
+
+#endif // MYTEST_H
