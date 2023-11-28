825ec6766edbf68291b912c0280cbb8e1629b246
Chakir Manyao Jija <84908567+Student5427@users.noreply.github.com>
2023-11-28 08:42:32 +0300

    Update myfunc.c

--- app/myfunc.c
+++ app/myfunc.c
@@ -1,17 +1,29 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-#include "myfunc.h"
 #include <math.h>
 
+#include "myfunc.h"
+
+void myfunc(double* roots, double a, double b, double c) {
+    double discriminant = b * b - 4 * a * c;
+    if (discriminant < 0) {
+	roots[0] = NAN;
+	roots[1] = NAN;
+    } else if (discriminant == 0) {
+        roots[0] = -b / (2 * a);
+	roots[1] = NAN;
+    } else {
+        double root1 = (-b + sqrt(discriminant)) / (2 * a);
+        double root2 = (-b - sqrt(discriminant)) / (2 * a);
+	roots[0] = root1;
+	roots[1] = root2;
+    }
+}
 int val;
 
 int fibonachi(int num) {
     int prev = 1;
     int next = 1;
-
-    if (num == 0)
-        return 1;
 
     if (num < 0)
         return 0;
@@ -29,13 +41,8 @@ int fibonachi(int num) {
     return next;
 }
 
-
-void find_roots(double* roots, double a, double b, double c) {    double discriminant = b * b - 4 * a * c;
-    if (discriminant < 0) { roots[0] = NAN;
- roots[1] = NAN;    } else if (discriminant == 0) {
-        roots[0] = -b / (2 * a); roots[1] = NAN;
-    } else {        double root1 = (-b + sqrt(discriminant)) / (2 * a);
-        double root2 = (-b - sqrt(discriminant)) / (2 * a); roots[0] = root1;
- roots[1] = root2;    }
+void printStdoutMessages() {
+    printf("This is a test message from myfunc.c\n");
+    printf("Do not disturb\n");
 }
 
